704287ddf56ab2132241133c73f150a8865e7095
Stefano <30950154+Stefifox@users.noreply.github.com>
2018-05-03 18:52:38 +0200

    Second Tutorial
    Updating file with new string

--- Tutorial/python/Bot_tutorial1.py
+++ Tutorial/python/Bot_tutorial1.py
@@ -1,13 +1,18 @@
 import time
 import telepot
 
-Token = telepot.Bot("Inserisci il token qui")
+Token = telepot.Bot("")
 
 def message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
 
     if(msg['text'] == "/start"):
         Token.sendMessage(chat_id, "Messaggio di start", parse_mode = "HTML")
+    if(msg['text'] == "/photo"):
+        Token.sendPhoto(chat_id, open("img.jpg", "rb"), "Didascalia")
+    if(msg['text'] == "/doc"):
+        Token.sendDocument(chat_id, open("doc.txt", "rb"), "Didascalia")
+        
 
 Token.message_loop(message)
 
