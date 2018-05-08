774deb42f193808f7c6e7d7e553c4d2501eaca9c
Stefano <30950154+Stefifox@users.noreply.github.com>
2018-05-08 16:33:25 +0200

    Tutorial #2
    Text Input and if

--- Tutorial/Java/Main.java
+++ Tutorial/Java/Main.java
@@ -1,10 +1,21 @@
 package tutorial;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[]args){
 		
-		System.out.println("Hello World");
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Benvenuto \nScrivi 'Ciao'");
+		
+		String Input = in.nextLine();
+		
+		if(Input.equals("Ciao")){
+			System.out.println("Hey!");
+		}
+		
 		
 	}
 	
