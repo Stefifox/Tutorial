e8278e1575ed087ea9bedc6650c746a0e98ecc5c
Stefano <30950154+Stefifox@users.noreply.github.com>
2018-05-10 16:17:08 +0200

    Tutorial #3
    Read Guide on StefifoxChannel (https://t.me/stefifoxchannel)

--- Tutorial/python/Bot_tutorial1.py
+++ Tutorial/python/Bot_tutorial1.py
@@ -12,7 +12,11 @@ def message(msg):
         Token.sendPhoto(chat_id, open("img.jpg", "rb"), "Didascalia")
     if(msg['text'] == "/doc"):
         Token.sendDocument(chat_id, open("doc.txt", "rb"), "Didascalia")
-        
+
+    if(msg['text'] == "/send"):
+        replyText = msg['reply_to_message']['text']
+        replyUser = msg['reply_to_message']['from']['username']
+        Token.sendMessage(chat_id, replyText + " \nFrom User: @"+replyUser)
 
 Token.message_loop(message)
 
